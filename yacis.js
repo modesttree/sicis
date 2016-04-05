cc3d2039ee13e19156868332c0c8109b91f33940
Steve Vermeulen <sfvermeulen@gmail.com>
2016-04-05 15:49:03 -0300

    Refactoring

--- yacis.js
+++ yacis.js
@@ -1,5 +1,84 @@
 
-var argv = require('minimist')(process.argv.slice(2));
+var portfinder = require('portfinder'),
+    httpServer = require('./lib/server'),
+    opener = require('opener'),
+    path = require('path'),
+    minimist = require('minimist');
+
+// This will add properties like ".cyan" to string
+require('colors');
+require('./lib/util');
+
+function findOpenPort(callback) {
+
+    portfinder.basePort = 8080;
+    portfinder.getPort(function (err, port) {
+
+        if (err) {
+            throw err;
+        }
+
+        callback(port);
+    });
+}
+
+function exitAllOnCtrlC() {
+    if (process.platform === 'win32') {
+        require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        }).on('SIGINT', function () {
+            process.emit('SIGINT');
+        });
+    }
+
+    process.on('SIGINT', function() {
+        _logger('http-server stopped.'.red);
+        process.exit();
+    });
+}
+
+function startApp() {
+
+    if (_options.desiredPort) {
+        startServer(_options.desiredPort);
+    }
+    else {
+        findOpenPort(function(port) {
+            startServer(port);
+        });
+    }
+
+    exitAllOnCtrlC()
+}
+
+function startServer(port) {
+
+    var serverOptions = {
+        rootPath: _options.rootPath,
+    };
+
+    var server = httpServer.start(serverOptions, _logger);
+
+    server.listen(port, function () {
+
+        _logger('Starting up yacis server at '.yellow
+            + server.rootPath.cyan
+            + ' with address '.yellow
+            + ('http://localhost:' + port).cyan);
+
+        _logger('Hit CTRL-C to stop the server');
+
+        if (_options.openBrowser) {
+            opener(
+                'http://localhost:' + port,
+                { command: null }
+            );
+        }
+    });
+}
+
+var argv = minimist(process.argv.slice(2));
 
 if (argv.h || argv.help) {
     console.log([
@@ -9,78 +88,20 @@ if (argv.h || argv.help) {
         "  -p                 Port to use [8080]",
         "  -s                 Suppress log messages from output",
         "  -d                 Do not open browser after starting the server",
-        "  -c                 Cache time (max-age) in seconds [3600], e.g. -c10 for 10 seconds.",
-        "                     To disable caching, use -c-1.",
         "",
         "  -h --help          Print this list and exit."
     ].join('\n'));
     process.exit();
 }
 
-var portfinder = require('portfinder'),
-    httpServer = require('./lib/server'),
-    opener = require('opener'),
-    path = require('path');
-
-// This will add properties like ".cyan" to string
-require('colors');
-
-var port = argv.p;
-
-var isSilent = argv.s || argv.silent;
-var logger = isSilent ? (function () {}) : console.log;
-
-if (!port) {
-    portfinder.basePort = 8080;
-    portfinder.getPort(function (err, port) {
-
-        if (err) {
-            throw err;
-        }
-
-        startServer(port);
-    });
-} else {
-    startServer(port);
+_options = {
+    desiredPort: argv.p,
+    isSilent: argv.s,
+    rootPath: path.dirname(process.argv[1]),
+    openBrowser: !argv.d,
 }
 
-process.on('SIGINT', function() {
-    logger('http-server stopped.'.red);
-    process.exit();
-});
+_logger = _options.isSilent ? (function () {}) : console.log;
 
-function startServer(port) {
-
-    var options = {
-        root: path.dirname(process.argv[0]),
-        cache: argv.c,
-        logger: logger,
-    };
-
-    if (!isSilent) {
-        options.onRequest = function(req, res) {
-            //var date = (new Date).toUTCString();
-            //logger('[%s] "%s %s" "%s"', date, req.method.cyan, req.url.cyan, req.headers['user-agent']);
-        };
-    };
-
-    var server = httpServer.createServer(options);
-
-    server.listen(port, function () {
-
-        logger('Starting up yacis server, serving '.yellow
-            + server.root.cyan
-            + ' on: '.yellow
-            + ('http://localhost:' + port).cyan);
-
-        logger('Hit CTRL-C to stop the server');
-
-        if (!argv.d) {
-            opener(
-                'http://localhost:' + port,
-                { command: null }
-            );
-        }
-    });
-}
+startApp()
 
